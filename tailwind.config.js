a0cbdbb2e3c979a9c912e4834593e44c05a67e99
Cosmeak <guillaumefine09@gmail.com>
2022-04-02 10:32:55 +0200

    :hammer:

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [ "./*.html" ],
+  theme: {
+    extend: {
+      colors: {
+        "success-white": "#F3F3F3",
+        "success-black": "#595959",
+        "success-orange": "#FF6400",
+      },
+      fontSize: {
+        "10xl": ["10rem", { lineHeight: '1', }]
+      }
+    },
+  },
+  plugins: [],
+}
