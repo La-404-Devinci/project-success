85924bad5506bb5cbadda415f82a0b3bdb546c43
Cosmeak <guillaumefine09@gmail.com>
2022-04-11 16:23:08 +0200

    :sparkles: Add home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     'assets/js/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "success-white": "#F3F3F3",
+        "success-black": "#595959",
+        "success-orange": "#FF6400",
+      },
+      fontSize: {
+        "10xl": ["10rem", { lineHeight: '1', }]
+      }
+    },
   },
   plugins: [],
 }
