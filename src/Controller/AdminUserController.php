617a0aed8d3db8dfedbd4c0f93e620ede4acb082
Cosmeak <guillaumefine09@gmail.com>
2022-05-05 17:04:56 +0200

    :sparkles: Add Authorize and unauthorized button and function for administrators

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use JetBrains\PhpStorm\NoReturn;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,19 +20,6 @@ class AdminUserController extends AbstractController
         if($this->isGranted('ROLE_ADMIN')) {
             return $this->render('admin/users/index.html.twig', [
                 'users' => $userRepository->findAll()
-            ]);
-        }
-        else {
-            return $this->redirectToRoute('home');
-        }
-    }
-
-    #[Route('/{id}/edit', name: 'dashboard_users_edit')]
-    public function edit(User $user): Response
-    {
-        if($this->isGranted('ROLE_ADMIN')) {
-            return $this->render('admin/users/edit.html.twig', [
-                'user' => $user
             ]);
         }
         else {
@@ -54,5 +42,35 @@ class AdminUserController extends AbstractController
         }
     }
 
+    #[Route('/{id}/authorize', name: 'dashboard_users_authorize')]
+    public function authorize(User $user, EntityManagerInterface $manager, Request $request): Response
+    {
+        if($this->isGranted('ROLE_ADMIN')) {
+            if($this->isCsrfTokenValid('authorize'.$user->getId(), $request->request->get('_token'))) {
+                $user->setRoles(["ROLE_ADMIN"]);
+                $manager->persist($user);
+                $manager->flush();
+            }
+            return $this->redirectToRoute('dashboard_users_index', [], Response::HTTP_SEE_OTHER);
+        }
+        else {
+            return $this->redirectToRoute('dashboard');
+        }
+    }
 
+    #[Route('/{id}/unauthorized', name: 'dashboard_users_unauthorized')]
+    public function unauthorized(User $user, EntityManagerInterface $manager, Request $request): Response
+    {
+        if($this->isGranted('ROLE_ADMIN')) {
+            if($this->isCsrfTokenValid('unauthorized'.$user->getId(), $request->request->get('_token'))) {
+                $user->setRoles([]);
+                $manager->persist($user);
+                $manager->flush();
+            }
+            return $this->redirectToRoute('dashboard_users_index', [], Response::HTTP_SEE_OTHER);
+        }
+        else {
+            return $this->redirectToRoute('dashboard');
+        }
+    }
 }
