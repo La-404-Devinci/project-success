08ff934f4b5d20577297c4ba3d729a2bd5163d28
Cosmeak <guillaumefine09@gmail.com>
2022-05-14 15:48:24 +0200

    :construction: Dashboard resources

--- src/Form/ResourceType.php
+++ src/Form/ResourceType.php
@@ -3,10 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Resource;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ResourceType extends AbstractType
 {
@@ -14,8 +18,23 @@ class ResourceType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('link')
-            ->add('file', FileType::class)
+            ->add('description', CKEditorType::class, [
+                'required' => false
+            ])
+            ->add('link', UrlType::class, [
+                'required' => false
+            ])
+            ->add('file', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+                'label' => 'Upload a file',
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5120k',
+                        'maxSizeMessage' => 'Please upload a file thad didn\'t exceed 5Mo!',
+                    ])
+                ]
+            ])
         ;
     }
 
