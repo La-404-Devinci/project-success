d77539f4a93af017f7cf6aa4df1d606548927b2b
Cosmeak <guillaumefine09@gmail.com>
2022-06-04 22:20:42 +0200

    :recycle: FEAT clean Code

--- src/Controller/Admin/AdminPartnerController.php
+++ src/Controller/Admin/AdminPartnerController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\News;
 use App\Entity\Partner;
 use App\Form\PartnerType;
 use App\Repository\PartnerRepository;
-use App\Services\UploadFileHelper;
+use App\Services\UploadFileService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,30 +17,40 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/admin/partners')]
 class AdminPartnerController extends AbstractController
 {
+    public function __construct(
+        protected PartnerRepository $partnerRepository,
+        protected UploadFileService $uploadFileHelper,
+    ){}
+
+    public function isSuperAdmin()
+    {
+        if(!$this->isGranted('ROLE_SUPER_ADMIN')) {
+            return $this->redirectToRoute('dashboard');
+        }
+    }
+
     #[Route('/', name: 'dashboard_partners_index')]
     public function index(PartnerRepository $partnerRepository): Response
     {
+        $this->isSuperAdmin();
         $partners = $partnerRepository->findAll();
-
         return $this->render('admin/partners/index.html.twig', [
             'partners' => $partners,
         ]);
     }
 
     #[Route('/create', name: 'dashboard_partners_create')]
-    public function create(Request $request, EntityManagerInterface $manager, UploadFileHelper $uploadFileHelper): Response
+    public function create(Request $request, EntityManagerInterface $manager): Response
     {
+        $this->isSuperAdmin();
         $partner = new Partner();
         $form = $this->createForm(PartnerType::class, $partner);
         $form->handleRequest($request);
-
         if($form->isSubmitted() && $form->isValid()) {
-            $partner->setImage($uploadFileHelper->upload($form->get('image')->getData()));
-            $manager->persist($partner);
-            $manager->flush();
+            $partner->setImage($this->uploadFileHelper->upload($form->get('image')->getData()));
+            $this->partnerRepository->add($partner);
             return $this->redirectToRoute('dashboard_partners_index');
         }
-
         return $this->renderForm('admin/partners/create.html.twig', [
             'partner' => $partner,
             'form' => $form,
@@ -47,12 +58,12 @@ class AdminPartnerController extends AbstractController
     }
 
     #[Route('/{id}/delete', name: 'dashboard_partners_delete')]
-    public function delete(PartnerRepository $partnerRepository): Response
+    public function delete(Request $request, Partner $partner): Response
     {
-        $partners = $partnerRepository->findAll();
-
-        return $this->render('admin/partners/index.html.twig', [
-            'partners' => $partners,
-        ]);
+        $this->isSuperAdmin();
+        if($this->isCsrfTokenValid('delete'.$partner->getId(), $request->request->get('_token'))) {
+            $this->partnerRepository->remove($partner);
+        }
+        return $this->redirectToRoute('dashboard_partners_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
+use App\Repository\NewsRepository;
 use App\Repository\PartnerRepository;
+use App\Repository\ResourceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
+    // Home Page
     #[Route('/', name: 'home')]
     public function index(PartnerRepository $partnerRepository): Response
     {
@@ -16,5 +20,52 @@ class AppController extends AbstractController
         return $this->render('app/index.html.twig', [
             'partners' => $partners,
         ]);
+    }
+
+    // News Pages
+    #[Route('/news', name: 'news_index')]
+    public function newsIndex(NewsRepository $newsRepository): Response
+    {
+        $news = $newsRepository->findAll();
+        return $this->render('news/index.html.twig', [
+            'news' => $news,
+        ]);
+    }
+
+    #[Route('/news/{id}', name: 'news_show')]
+    public function newsCreate(News $news): Response
+    {
+        return $this->render('news/show.html.twig', [
+            'news' => $news,
+        ]);
+    }
+
+    // Media Page
+    #[Route('/media', name: 'media_index')]
+    public function mediaIndex(): Response
+    {
+        return $this->render('media/index.html.twig');
+    }
+
+    // Resources Page
+    #[Route('/resources', name: 'resources_index')]
+    public function resourcesIndex(ResourceRepository $resourceRepository): Response
+    {
+        $resources = $resourceRepository->findAll();
+        return $this->render('resources/index.html.twig', [
+            'resources' => $resources,
+        ]);
+    }
+
+    // Admin redirection
+    #[Route('/admin', name: 'dashboard')]
+    public function admin(): Response
+    {
+        if($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('dashboard_news_index');
+        }
+        else {
+            return $this->redirectToRoute('home');
+        }
     }
 }
